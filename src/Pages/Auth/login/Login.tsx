435f1f9e39c0ee0d9698cc576161f7d862a00105
bmalkes <bmalkes@gmail.com>
2023-03-01 12:15:15 +0200

    feat: login with firebase

--- src/Pages/Auth/login/Login.tsx
+++ src/Pages/Auth/login/Login.tsx
@@ -11,16 +11,38 @@ import {
 import { useForm } from "react-hook-form";
 import InputError from "../../../Components/input-error-msg/InputErrorMsg";
 import validator from "validator";
+import {
+  AuthError,
+  AuthErrorCodes,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../../script/firebase.config";
 
 const Login = () => {
   const {
     handleSubmit,
     register,
+    setError,
     formState: { errors },
   } = useForm();
 
-  const handleSubmitForm = (data: any) => {
-    console.log({ data });
+  const handleSubmitForm = async (data: any) => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      console.log(userCredentials);
+    } catch (err) {
+      const _error = err as AuthError;
+      if (_error.code === AuthErrorCodes.INVALID_PASSWORD) {
+        return setError("password", { type: "mismatch" });
+      }
+      if (_error.code === AuthErrorCodes.USER_DELETED) {
+        return setError("email", { type: "notFound" });
+      }
+    }
   };
 
   console.log({ errors });
@@ -56,6 +78,9 @@ const Login = () => {
           {errors?.email?.type === "validate" && (
             <InputError>Is not a valid email</InputError>
           )}
+          {errors?.email?.type === "notFound" && (
+            <InputError>Email not found</InputError>
+          )}
         </LoginInputContainer>
         <LoginInputContainer>
           <p>Password</p>
@@ -71,6 +96,9 @@ const Login = () => {
           ></CustomInput>
           {errors?.password?.type === "required" && (
             <InputError>Password Required</InputError>
+          )}
+          {errors?.password?.type === "mismatch" && (
+            <InputError>Password wrong</InputError>
           )}
         </LoginInputContainer>
         <Button
