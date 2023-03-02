44bba95213f9b11f1d7a83ad7e603175404e744c
bmalkes <bmalkes@gmail.com>
2023-03-02 17:15:49 +0200

    fix

--- src/Components/CoffeeSquare/index.tsx
+++ src/Components/CoffeeSquare/index.tsx
@@ -1,5 +1,5 @@
 import { SquareContainer } from "./styles";
-import CoffeeItem from "../coffeeitem/CoffeeItem";
+import CoffeeItem from "../CoffeeItem";
 import { useAppSelector } from "../../store/store";
 
 export const CoffeeSection = () => {
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { BannerHome } from "../../Components/Banner";
 
 import { HomeContainer, SquareCoffee } from "./styles";
 import { useTranslation } from "react-i18next";
-import { CoffeeSection } from "../../Components/coffeesquare/CoffeeSquare";
+import { CoffeeSection } from "../../Components/CoffeeSquare";
 const Home = () => {
   const { t } = useTranslation();
   return (
