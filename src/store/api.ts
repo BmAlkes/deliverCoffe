68ebe52a0fbcb26a58b17f5037fd6bfb61ee3250
bmalkes <bmalkes@gmail.com>
2023-05-29 11:09:43 +0300

    feat:add refatoring fetch

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,12 +1,27 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 import { Product } from "../@types/product";
 import { db } from "../script/firebase.config";
 
 export const fetchProducts = async () => {
-  const productsFromFirestore: Product[] = [];
-  const querySnapshot = await getDocs(collection(db, "products"));
-  querySnapshot.forEach((doc: any) => {
-    productsFromFirestore.push(doc.data());
+  return await fetchDataFromFirebase<Product>("products");
+  // const productsFromFirestore: Product[] = [];
+  // const querySnapshot = await getDocs(collection(db, "products"));
+  // querySnapshot.forEach((doc: any) => {
+  //   productsFromFirestore.push(doc.data());
+  // });
+  // return productsFromFirestore;
+};
+
+const fetchDataFromFirebase = async <T>(path: string): Promise<T[]> => {
+  const listFromFirebase: T[] = [];
+  const querySnapshot = await getDocs(collection(db, path));
+  querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData>) => {
+    listFromFirebase.push(doc.data() as T);
   });
-  return productsFromFirestore;
+  return listFromFirebase;
 };
